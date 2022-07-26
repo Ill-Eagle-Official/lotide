d05ceca1f89ed64a1e70c42a8b5daa93fcf87355
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-07-26 23:10:31 +0000

    taking elements out of arrays

---
+++ without.js
@@ -0,0 +1,28 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("Yeah, that's some equal stuff right there.")
+  } else {
+    console.log("Nope, that ain't equal.")
+  }
+};
+
+const without = function(source, toRemove) {
+  return source.filter(leaveIt => leaveIt != toRemove);
+};
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
