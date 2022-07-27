ef2d82b520418b768d8ca547751dba1687cc7a63
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-07-27 22:09:49 +0000

    added finding properties by key value in lotide

---
+++ findByKeyValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function findKeyByValue(object, value) {
+  let keyArray = Object.keys(object)
+  for (let property of keyArray) {
+    if (object[property] === value) {
+      return property;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
