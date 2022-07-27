28c7cafcae74689a2f0dfe242beb5a7348a165c4
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-07-27 21:19:08 +0000

    added letter counting function

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function countLetters(string) {
+  let result = {};
+
+  for (let letter of string) {
+    if (string[letter] !== ' ') {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countLetters('my name jeff'));
